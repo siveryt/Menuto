39ce04687c170d58762704f5d3e12681d43acd86
Sivery <50551841+siveryt@users.noreply.github.com>
2024-09-20 22:46:12 +0200

    Initial Commit

---
+++ Menuto/MenutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenutoApp.swift
+//  Menuto
+//
+//  Created by Christian Nagel on 20.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MenutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
