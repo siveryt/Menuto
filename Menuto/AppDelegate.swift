1f81af7a0e263e9c25dd54e238c0337b704da2ad
Sivery <50551841+siveryt@users.noreply.github.com>
2024-09-21 18:36:40 +0200

    Basic Functionality working

---
+++ Menuto/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Menuto
+//
+//  Created by Christian Nagel on 20.09.24.
+//
+
+import Foundation
--- Menuto/MenutoApp.swift
+++ Menuto/MenutoApp.swift
@@ -9,9 +9,50 @@ import SwiftUI
 
 @main
 struct MenutoApp: App {
+    @AppStorage("setMinutes") var setMinutes: Int = 5;
+    
+    @State private var timeRemaining: Int = 60 // Timer duration in seconds
+    @State private var running: Bool = false;
+    
+    var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        MenuBarExtra(String(format: "%02d:%02d", timeRemaining / 60, timeRemaining % 60)) {
+            VStack {
+                
+                LabeledContent {
+                    TextField("Enter number", text: Binding(
+                        get: { String(self.setMinutes) },
+                        set: { newValue in
+                            if let intValue = Int(newValue) {
+                                self.setMinutes = intValue
+                            } else {
+                                self.setMinutes = 0
+                            }
+                        }
+                    ))
+                } label: {
+                    Text("Minutes")
+                }
+                
+                Divider()
+                HStack {
+                    Button("Quit") {
+                        NSApplication.shared.terminate(nil)
+                    }.keyboardShortcut("q")
+                    Spacer()
+                    Button("Start") {
+                        running = true;
+                    }
+                }
+            }
+            .padding()
+            .onReceive(timer) { input in
+                if (!running) { return };
+                timeRemaining -= 1
+            }
+            
+        }.menuBarExtraStyle(.window)
     }
+    
 }
